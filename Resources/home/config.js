fb0e1bd53676939b18a166a54417f969459b0136
unknown <yoshiki@dosv-yoshiki5.jp.co.square-enix.com>
2012-12-13 14:27:15 +0900

    aa

---
+++ Resources/home/config.js
@@ -0,0 +1,12 @@
+
+function ConfigScene(title)
+{
+	var window = Ti.UI.createWindow({
+		backgroundColor:'white',
+		title : title
+	});	
+	
+return window;
+};
+
+module.exports = ConfigScene;
---
+++ Resources/home/shop.js
@@ -0,0 +1,12 @@
+
+function ShopScene(title)
+{
+	var window = Ti.UI.createWindow({
+		backgroundColor:'white',
+		title : title
+	});	
+	
+return window;
+};
+
+module.exports = ShopScene;
